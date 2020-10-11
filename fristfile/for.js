95aac9942ad5b90ca66afc59eac2b492d8f79601
prabu 007 <58351074+prabu72890@users.noreply.github.com>
2020-10-11 20:50:37 +0530

    covid check
    I practice today
    for loop and 
    for array

---
+++ fristfile/for.js
@@ -0,0 +1,21 @@
+var arrayCity = ["colombo", "kalutara", "jaffna", "horana", "galle","matara"];
+
+
+for (var j=0;j<arrayCity.length;j++){
+    console.log(arrayCity[j] + " "+ "city");
+}
+
+
+
+for (var i=0;i<11;i++){
+    console.log("hello");
+
+}
+
+for (var i=6;i<11;i++){
+    console.log("five");
+
+}
+
+
+
